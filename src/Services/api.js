ee8cd83eef20d52f81c57483abafa5923713d64a
Emmanuel <basikolo24emmauel@gmail.com>
2025-07-01 17:58:21 +0200

    debug

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/',
-  // baseURL: 'https://band-register-drf.onrender.com/',
+  // baseURL: 'http://localhost:8000/',
+  baseURL: 'https://band-register-drf.onrender.com/',
   withCredentials: true,
 });
 
